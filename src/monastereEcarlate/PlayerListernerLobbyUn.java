b22a05dd727cee866c2370be83fa6d44a3f2fe51
Falcort <falcort.jeux@gmail.com>
2016-05-23 17:03:27 +0200

    Ajout pancarte "Quittez"
    Ajout de la pancarte quitter, et du placement des pancarte au démmarage
    du serveur au cas ou elles auraient été casser

--- src/monastereEcarlate/PlayerListernerLobbyUn.java
+++ src/monastereEcarlate/PlayerListernerLobbyUn.java
@@ -3,6 +3,7 @@ package monastereEcarlate;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -25,7 +26,7 @@ public class PlayerListernerLobbyUn implements Listener
 	
 	//Nombre de joueur dans la zone
 	private int numberPlayerInArea;
-	
+
 	//Coor de la sign
 	private int sx = -216;
 	private int sy = 71;
@@ -36,9 +37,23 @@ public class PlayerListernerLobbyUn implements Listener
 	private int tpsy = 70;
 	private int tpsz = 243;
 	
+	//Coord Sign de retour
+	private int tpsbx = -216;
+	private int tpsby = 71;
+	private int tpsbz = 242;
+	
+	//Cord de Tp de la sign de retour
+	private int tpbx = -217;
+	private int tpby = 84;
+	private int tpbz = 253;
+	
+	private MonastereEcarlate plugin;
+	
 	public PlayerListernerLobbyUn(MonastereEcarlate plugin)
 	{
+		this.plugin = plugin;
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+		placeSign();
 	}
 	
 	@EventHandler
@@ -61,10 +76,38 @@ public class PlayerListernerLobbyUn implements Listener
 		numberPlayerInArea = 0;
 	}
 	
+	@SuppressWarnings("deprecation")
+	public void placeSign()
+	{
+		Location SignEnterLoc = new Location(Bukkit.getServer().getWorld("world"), sx, sy, sz);
+		Location SignExitLoc = new Location(Bukkit.getServer().getWorld("world"), tpsbx, tpsby, tpsbz);
+		
+		SignEnterLoc.getBlock().setType(Material.WALL_SIGN);
+		SignExitLoc.getBlock().setType(Material.WALL_SIGN);
+		SignExitLoc.getBlock().setData( (byte) 0x03 );
+		
+		String UpdateBack = "blockdata " + tpsbx + " " + tpsby + " " + tpsbz + " {Text2:\"{\\\"text\\\":\\\"Quittez ?\\\",\\\"clickEvent\\\":{\\\"action\\\":\\\"run_command\\\",\\\"value\\\":\\\"tp @p " + tpbx + " " + tpby + " " + tpbz + "\\\"}}\"}"; 
+		
+		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this.plugin, new Runnable()
+		{
+			public void run()
+			{
+				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), UpdateBack);
+			}
+		}, 60);
+	}
+	
 	public void updateSign()
 	{
-		String commande = "blockdata " + sx + " " + sy + " " + sz + " {Text1:\"{\\\"text\\\":\\\"Monastère\\\",\\\"color\\\":\\\"red\\\"}\", Text2:\"{\\\"text\\\":\\\"Instance n°1\\\",\\\"color\\\":\\\"green\\\"}\", Text3:\"{\\\"text\\\":\\\"" + numberPlayerInArea + " / " + nbPlayerInstance + "\\\", \\\"color\\\":\\\"aqua\\\"}\", Text4:\"{\\\"text\\\":\\\"Rejoindre ?\\\", \\\"color\\\":\\\"green\\\", \\\"clickEvent\\\":{\\\"action\\\":\\\"run_command\\\",\\\"value\\\":\\\"tp @p " + tpsx + " " + tpsy + " " + tpsz + "\\\"}}\"}";
-		Bukkit.broadcastMessage(commande);
-		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), commande);
+		String Update = "blockdata " + sx + " " + sy + " " + sz + " {Text1:\"{\\\"text\\\":\\\"Monastère\\\",\\\"color\\\":\\\"red\\\"}\", Text2:\"{\\\"text\\\":\\\"Instance n°1\\\",\\\"color\\\":\\\"green\\\"}\", Text3:\"{\\\"text\\\":\\\"" + numberPlayerInArea + " / " + nbPlayerInstance + "\\\", \\\"color\\\":\\\"aqua\\\"}\", Text4:\"{\\\"text\\\":\\\"Rejoindre ?\\\", \\\"color\\\":\\\"green\\\", \\\"clickEvent\\\":{\\\"action\\\":\\\"run_command\\\",\\\"value\\\":\\\"tp @p " + tpsx + " " + tpsy + " " + tpsz + "\\\"}}\"}";
+		String UpdateFull = "blockdata " + sx + " " + sy + " " + sz + " {Text1:\"{\\\"text\\\":\\\"Monastère\\\",\\\"color\\\":\\\"red\\\"}\", Text2:\"{\\\"text\\\":\\\"Instance n°1\\\",\\\"color\\\":\\\"green\\\"}\", Text3:\"{\\\"text\\\":\\\"" + numberPlayerInArea + " / " + nbPlayerInstance + "\\\", \\\"color\\\":\\\"red\\\"}\", Text4:\"{\\\"text\\\":\\\"COMPLET\\\", \\\"color\\\":\\\"red\\\"}\"}";
+		if (numberPlayerInArea < nbPlayerInstance)
+		{
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), Update);
+		}
+		else
+		{
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), UpdateFull);
+		}
 	}
 }
